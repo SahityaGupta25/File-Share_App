92f4e6bb8e71b6dbcca4bbf9f2b44978eda4ad99
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-19 13:18:19 +0530

    files

--- Back-hend/server.js
+++ Back-hend/server.js
@@ -1,10 +1,19 @@
+// !  IMPORTED
+
 const express = require('express')  ;
 const app = express();
+
+const connectDB = require('./config/db');
+// !-------------------------------------------------------------------------
+
 
 
 // ^  PORT
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
